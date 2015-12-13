5a1660f05f2266001f62c59f72766ad3a91e5923
Manuel Vonthron <manuel.vonthron@acadis.org>
2015-12-12 21:50:58 -0500

    changed threading (better perf)

--- plot.py
+++ plot.py
@@ -2,26 +2,30 @@ import threading
 import time
 from queue import Queue
 
+import numpy as np
 from PySide import QtGui, QtCore
 import pyqtgraph as pg
 
-class Watcher(QtCore.QThread):
-    def __init__(self, parent, queue):
+
+class DataInput(QtCore.QThread):
+    source = QtCore.Signal(object)
+    def __init__(self):
         super().__init__()
-        self.parent = parent
-        self.queue = queue
+        # self.source = QtCore.Signal(int)
 
     def run(self):
-        while True:
-            i = self.queue.get()
-            print("received", i)
-            self.queue.task_done()
-            self.parent.new_data(i)
+        for i in range(1000):
+            val = np.random.rand()
+            # print(i, "->", val)
+            self.source.emit(val)
+            time.sleep(0.01)
 
-class Plotter(threading.Thread):
-    def __init__(self, queue):
+class Plotter(QtCore.QObject):
+    def __init__(self):
         super().__init__()
-        self.watcher = Watcher(self, queue)
+        self.dataIn = DataInput()
+        self.dataIn.source.connect(self.new_data)
+
 
     def setup(self):
         self.app = QtGui.QApplication([])
@@ -38,35 +42,33 @@ class Plotter(threading.Thread):
         self.plot_widget = pg.PlotWidget(name='Plot1')
         self.layout.addWidget(self.plot_widget)
 
-        y = [0, 0.1, 0.2, 0.001, 0.3]
-        self.plot = self.plot_widget.plot(y=y, antialias=True, pen={'color': 'F66'})
+        self.y = [0, 0.1, 0.2, 0.001, 0.3]
+        self.plot = self.plot_widget.plot(y=self.y, antialias=True, pen={'color': 'F66'})
 
         self.plot_widget.setLabel('left', 'Value', units='V')
         self.plot_widget.setLabel('bottom', 'Time', units='s')
-        self.plot_widget.setXRange(0, 6)
-        self.plot_widget.setYRange(0, 1)
+        # self.plot_widget.setXRange(0, 100)
+        # self.plot_widget.setYRange(0, 1)
+
+        self.dataIn.start()
 
     def run(self):
         self.setup()
-        self.watcher.start()
 
         self.win.show()
         self.app.exec_()
 
+    @QtCore.Slot(object)
     def new_data(self, i):
-        print("Received new data:", i)
+        # print(" received ->", i)
+        self.y.append(i)
+        self.plot.setData(y=self.y)
 
 
 if __name__ == '__main__':
-    queue = Queue()
+    p = Plotter()
+    p.run()
 
-    p = Plotter(queue)
-    p.start()
-
-    print('launched')
-    for i in range(10, 0, -1):
-        queue.put(i)
-        time.sleep(0.5)
 
 
 
